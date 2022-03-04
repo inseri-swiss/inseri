37ea153a0ac8ebe7679eeb4dcc613c973292223f
Arooran Thanabalasingam <arooran.thanabalasingam@uzh.ch>
2022-03-04 20:35:22 +0100

    fix some tests

--- frontend/src/app/query-app-interface/data-management/data-management/data-management.component.spec.ts
+++ frontend/src/app/query-app-interface/data-management/data-management/data-management.component.spec.ts
@@ -1,4 +1,13 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { MatDialogModule, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { ActivatedRoute } from '@angular/router';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { of } from 'rxjs';
+import { ActionService } from 'src/app/user-action-engine/mongodb/action/action.service';
+import { OpenAppsModel } from 'src/app/user-action-engine/mongodb/page/open-apps.model';
+import { PageService } from 'src/app/user-action-engine/mongodb/page/page.service';
+import { QueryService } from 'src/app/user-action-engine/mongodb/query/query.service';
 
 import { DataManagementComponent } from './data-management.component';
 
@@ -8,7 +17,25 @@ describe('DataManagementComponent', () => {
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [ DataManagementComponent ]
+      imports: [MatDialogModule, HttpClientTestingModule],
+      declarations: [ DataManagementComponent ],
+      providers: [
+        NgxSpinnerService,
+        ActionService,
+        PageService,
+        QueryService,
+        OpenAppsModel,
+        {provide: MAT_DIALOG_DATA, useValue: [
+          {},
+          {appInputQueryMapping: []},
+          []
+        ]},
+        {provide: MatDialogRef, useValue: {}},
+        {provide: QueryService, useValue: {
+          getAllQueriesOfPage: () => of()
+        }},
+        {provide: ActivatedRoute, useValue: {}},
+      ]
     })
     .compileComponents();
   }));

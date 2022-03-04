fd018c2b06f57d03d3d5f2972244d1203f59b967
Arooran Thanabalasingam <arooran.thanabalasingam@uzh.ch>
2022-03-04 20:35:22 +0100

    fix some tests

--- frontend/src/app/query-engine/general/general-request.service.spec.ts
+++ frontend/src/app/query-engine/general/general-request.service.spec.ts
@@ -1,11 +1,19 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed, inject } from '@angular/core/testing';
+import { ActivatedRoute } from '@angular/router';
+import { QueryService } from 'src/app/user-action-engine/mongodb/query/query.service';
 
 import { GeneralRequestService } from './general-request.service';
 
 describe('GeneralRequestService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [GeneralRequestService]
+      imports: [HttpClientTestingModule],
+      providers: [
+        GeneralRequestService,
+        QueryService,
+        {provide: ActivatedRoute, useValue: {}}
+      ]
     });
   });
 
